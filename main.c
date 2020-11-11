d2cbf89aa504001fc674a5236e9589768441bd11
jnannie <jnannie@student.21-school.ru>
2020-11-12 02:57:39 +0300

    first version

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <unistd.h>
 #include <string.h>
+// #include <stdio.h>
 
 int			g_pipe_des[2];
 int			g_stdout;
@@ -33,8 +34,7 @@ char		*ft_strdup(char *str)
 
 void		fatal_error(void)
 {
-	write(2, "error: fatal", ft_strlen("error: fatal"));
-	write(2, "\n", 1);
+	write(2, "error: fatal\n", ft_strlen("error: fatal\n"));
 	exit(EXIT_FAILURE);
 }
 
@@ -55,64 +55,10 @@ char		**add_arg(char **command_argv, char *arg)
 		i++;
 	}
 	free(temp);
-	command_argv[i] = ft_strdup(arg);
+	if (!(command_argv[i] = ft_strdup(arg)))
+		fatal_error();
 	command_argv[i + 1] = 0;
 	return (command_argv);
-}
-
-void		execute(char **command_argv, char **envp, int in_pipe)
-{
-	int		pid;
-
-	if (g_pipe_des[0])
-	{
-		if (dup2(g_pipe_des[0], 0) == -1)
-			fatal_error();
-		close(g_pipe_des[0]);
-		g_pipe_des[0] = 0;
-	}
-	else if (dup2(g_stdin, 0) == -1)
-			fatal_error();
-	if (in_pipe)
-	{
-		if (pipe(g_pipe_des) == -1)
-			fatal_error();
-		if (dup2(g_pipe_des[1], 1) == -1)
-			fatal_error();
-		close(g_pipe_des[1]);
-	}
-	else if (dup2(g_stdout, 1) == -1)
-			fatal_error();
-	if (!command_argv)
-		return ;
-	if (strcmp(command_argv[0], "cd") == 0)
-	{
-		if (!command_argv[1] || command_argv[2])
-			write(2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
-		else if (chdir(command_argv[1]) == -1)
-		{
-			write(2, "error: cd: cannot change directory to ", ft_strlen("error: cd: cannot change directory to "));
-			write(2, command_argv[1], ft_strlen(command_argv[1]));
-			write(2, "\n", 1);
-		}
-	}
-	else
-	{
-		pid = fork();
-		if (pid < 0)
-			fatal_error();
-		else if (pid == 0)
-		{
-			execve(command_argv[0], command_argv, envp);
-			write(2, "error: cannot execute ", ft_strlen("error: cannot execute "));
-			write(2, command_argv[0], ft_strlen(command_argv[0]));
-			write(2, "\n", 1);
-			exit(-1);
-		}
-	}
-	if (!in_pipe)
-		while (wait(NULL) != -1)
-			;
 }
 
 void		free_argv(char **command_argv)
@@ -127,42 +73,110 @@ void		free_argv(char **command_argv)
 	free(command_argv);
 }
 
+int			execute(char **command_argv, char **envp, int in_pipe)
+{
+	int		pid;
+	int		exit_status = 0;
+	int		ret = 0;
+
+	if (strcmp(command_argv[0], "cd") == 0)
+	{
+		if (!command_argv[1] || command_argv[2])
+		{
+			write(2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
+			ret = 1;
+		}
+		else if ((chdir(command_argv[1])) == -1)
+		{
+			write(2, "error: cd: cannot change directory to ", ft_strlen("error: cd: cannot change directory to "));
+			write(2, command_argv[1], ft_strlen(command_argv[1]));
+			write(2, "\n", 1);
+			ret = 1;
+		}
+	}
+	else
+	{
+		if (g_pipe_des[0])
+		{
+			if (dup2(g_pipe_des[0], 0) == -1)
+				fatal_error();
+			close(g_pipe_des[0]);
+			g_pipe_des[0] = 0;
+		}
+		else if (dup2(g_stdin, 0) == -1)
+			fatal_error();
+		if (in_pipe)
+		{
+			if (pipe(g_pipe_des) == -1)
+				fatal_error();
+			if (dup2(g_pipe_des[1], 1) == -1)
+				fatal_error();
+			close(g_pipe_des[1]);
+		}
+		else if (dup2(g_stdout, 1) == -1)
+				fatal_error();
+		pid = fork();
+		if (pid < 0)
+			fatal_error();
+		else if (pid == 0)
+		{
+			execve(command_argv[0], command_argv, envp);
+			write(2, "error: cannot execute ", ft_strlen("error: cannot execute "));
+			write(2, command_argv[0], ft_strlen(command_argv[0]));
+			write(2, "\n", 1);
+			exit(127);
+		}
+		waitpid(pid, &exit_status, 0);
+		if (WIFEXITED(exit_status))
+			ret = WEXITSTATUS(exit_status);
+		// else if (WIFSIGNALED(exit_status))
+		// 	exit_status = exit_status | 128;
+		// 	exit_status = WTERMSIG(exit_status);
+	}
+	return (ret);
+}
+
 int			main(int argc, char **argv, char **envp)
 {
 	char	**command_argv;
+	int		ret = 0;
 
 	command_argv = 0;
-	argv++;
 	g_stdin = dup(0);
 	g_stdout = dup(1);
-	while (1)
+	while (argc)
 	{
+		argv++;
 		if (*argv == 0)
 		{
 			if (command_argv)
-				execute(command_argv, envp, 0);
+				ret = execute(command_argv, envp, 0);
 			free_argv(command_argv);
+			command_argv = 0;
 			break ;
 		}
-		else if (**argv == '|' && *(*argv + 1) == '\0')
+		else if (!strcmp(*argv, "|"))
 		{
-			execute(command_argv, envp, 1);
+			if (command_argv)
+				ret = execute(command_argv, envp, 1);
 			free_argv(command_argv);
 			command_argv = 0;
 		}
-		else if (**argv == ';' && *(*argv + 1) == '\0')
+		else if (!strcmp(*argv, ";"))
 		{
-			execute(command_argv, envp, 0);
+			if (command_argv)
+				ret = execute(command_argv, envp, 0);
 			free_argv(command_argv);
 			command_argv = 0;
 		}
 		else
 			command_argv = add_arg(command_argv, *argv);
-		argv++;
 	}
 	dup2(g_stdin, 0);
 	dup2(g_stdout, 1);
 	close(g_stdin);
 	close(g_stdout);
-	return (!argc);
+	// pipe(g_pipe_des);
+	// printf("%d %d\n", g_pipe_des[0], g_pipe_des[1]);
+	return (ret);
 }
